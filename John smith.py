82cea041c3b7c8e7b3be723903a650679c3c9727
swathi ungrapalli <swathins1998@outlook.com>
2023-04-27 17:50:55 -0400

    Adding another python file

---
+++ John smith.py
@@ -0,0 +1,69 @@
+#Printing customer's data
+"""first_name="John Smith"
+age=20
+Status="New Patient"
+print(first_name,age, Status)"""
+
+#Input function program
+"""Name= input("What is your name? ")
+print("Hello "+Name)"""
+
+#Print Integer & string values"
+"""birth_year=input(" What is your birth year")
+age=2023-int(birth_year)
+message="Age " + str(age)
+print(message)"""
+
+#print sum of 2 numbers given one of the number is decimal
+"""a=int(input("Enter first number: "))
+b=float(input("Enter second number: "))
+sum=a+b
+message="Sum of 2 numbers is: " + str(sum)
+print(message)"""
+
+#String ex for object string method variable
+"""course="swathi"
+print(course.find('t'))
+print("Swa" in course)
+print(course.replace('w','4'))"""
+
+#Logical opereators
+"""
+A=0
+print(A>1 or A<10)"""
+
+#If statements
+"""love = 200
+
+if love<100:
+    print("He really loves me")
+    print("Give him some flowers")
+    
+print("Done")"""
+
+#Weight connversion from kg to lbs
+"""weight=(input("Enter weight: "))
+if weight.isnumeric():
+    value=input("(K)g or (L)bs: ")
+    if value.upper()=="K":
+        converted=(float(weight)*2.20)
+        print("Weight in lbs: ",converted)
+    elif value.upper()=="L":
+        converted=(float(weight)/2.20)
+        print("Weight in kg: ",converted)
+    else:
+        print("Invalid value")
+else:
+    print("Number only allowed")"""
+
+
+
+
+
+
+
+
+
+
+
+
